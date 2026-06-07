33c57f2154dddb94ba82a2236d9540ef38dc45cd
Michael Phelps <mpphelps@gmail.com>
2026-06-07 18:37:29 -0400

    Add favicon.svg and update links for improved branding

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -5,6 +5,7 @@ import { RouteErrorPanel } from "~/components/layout/route-error-panel";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
+  { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
